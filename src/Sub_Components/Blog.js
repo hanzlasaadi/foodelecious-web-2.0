7febbda234ebc767933a8406f012a264038eeebf
asmaqamar1111 <asmaqamar1111@gmail.com>
2023-12-06 18:29:10 +0500

    changes by asma

--- src/Sub_Components/Blog.js
+++ src/Sub_Components/Blog.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Blog = () => {
   return (
     <div>
       {/* Why Choose */}
-      <div className="row d-flex justify-center items-center p-5 ">
+      <div className="row d-flex justify-center items-center m-5 ">
         <h1 className="text-dark mt-5 font-sans font-bold text-center">
           Why Choose Us
         </h1>
@@ -68,66 +69,128 @@ const Blog = () => {
       </div>
 
       {/* Blog  */}
-      <div className="row d-flex justify-center items-center p-5 ">
+      <div className="row d-flex justify-center items-center m-5 ">
         {" "}
         <h1 className="text-dark mt-5 font-sans font-bold text-left">Blog</h1>
-        <CardGroup>
-          <Card>
-            <Card.Img
-              variant="top"
-              src="https://ik.imagekit.io/2nuimwatr/Blog1.png?updatedAt=1701786962110"
-            />
-            <Card.Body>
-              <Card.Text class="text-slate-600">
-                Great food and reasonable prices. recommended to best time with
-                Great customer service Great food and reasonable prices.
-                recommended to best time with Great customer service Great food
-                and reasonable prices. recommended to best time with Great
-                customer service...
-              </Card.Text>
-              <Card.Text class="text-right text-slate-600">
-                Read More &gt;&gt;
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Img
-              variant="top"
-              src="https://ik.imagekit.io/2nuimwatr/blog2.png?updatedAt=1701786962088"
-            />
-            <Card.Body>
-              <Card.Text class="text-slate-600">
-                Great food and reasonable prices. recommended to best time with
-                Great customer service Great food and reasonable prices.
-                recommended to best time with Great customer service Great food
-                and reasonable prices. recommended to best time with Great
-                customer service...
-              </Card.Text>
-              <Card.Text class="text-right text-slate-600">
-                Read More &gt;&gt;
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Img
-              variant="top"
-              src="https://ik.imagekit.io/2nuimwatr/blog3.png?updatedAt=1701786962017"
-            />
-            <Card.Body>
-              <Card.Text class="text-slate-600">
-                Great food and reasonable prices. recommended to best time with
-                Great customer service Great food and reasonable prices.
-                recommended to best time with Great customer service Great food
-                and reasonable prices. recommended to best time with Great
-                customer service...
-              </Card.Text>
-              <Card.Text class="text-right text-slate-600">
-                Read More &gt;&gt;
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </CardGroup>
+        <Container fluid className="my-16 lg:px-32 xxl:px-32 md:px-20">
+          <Row className="rounded-[0px]">
+            <Col md={4} lg={4} xl={4} xxl={4} xs={12}>
+              <Card class="p-3 bg-white shadow-md border  rounded-lg">
+                <Card.Img
+                  variant="top"
+                  src="https://ik.imagekit.io/2nuimwatr/Blog1.png?updatedAt=1701786962110"
+                  class="p-3"
+                />
+                <Card.Body>
+                  <Card.Text class="text-slate-600 text-left">
+                    Great food and reasonable prices. recommended to best time
+                    with Great customer service Great food and reasonable
+                    prices. recommended to best time with Great customer service
+                    Great food and reasonable prices. recommended to best time
+                    with Great customer service...
+                  </Card.Text>
+                  <Card.Text class="text-right text-slate-600 cursor-pointer">
+                    Read More &gt;&gt;
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4} lg={4} xl={4} xxl={4} xs={12}>
+              <Card class="p-3 bg-white shadow-md border  rounded-lg">
+                <Card.Img
+                  variant="top"
+                  src="https://ik.imagekit.io/2nuimwatr/blog2.png?updatedAt=1701786962088"
+                  class="p-3"
+                />
+                <Card.Body>
+                  <Card.Text class="text-slate-600 text-left">
+                    Great food and reasonable prices. recommended to best time
+                    with Great customer service Great food and reasonable
+                    prices. recommended to best time with Great customer service
+                    Great food and reasonable prices. recommended to best time
+                    with Great customer service...
+                  </Card.Text>
+                  <Card.Text class="text-right text-slate-600 cursor-pointer">
+                    Read More &gt;&gt;
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4} lg={4} xl={4} xxl={4} xs={12}>
+              <Card class="p-3 bg-white shadow-md border  rounded-lg">
+                <Card.Img
+                  variant="top"
+                  src="https://ik.imagekit.io/2nuimwatr/blog3.png?updatedAt=1701786962017"
+                  class="p-3"
+                />
+                <Card.Body>
+                  <Card.Text class="text-slate-600 text-left">
+                    Great food and reasonable prices. recommended to best time
+                    with Great customer service Great food and reasonable
+                    prices. recommended to best time with Great customer service
+                    Great food and reasonable prices. recommended to best time
+                    with Great customer service...
+                  </Card.Text>
+                  <Card.Text class="text-right text-slate-600 cursor-pointer">
+                    Read More &gt;&gt;
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
+
+      {/* contact */}
+      <Container fluid class="p-0" style={{padding: 0}}>
+        <div className="d-flex ">
+          <div className="col-lg-6 p-5 m-0  bg-[#FFF4D9] d-flex flex-col justify-center items-center ">
+            <h1 className="text-dark  font-sans font-bold text-center">
+              Visit Us
+            </h1>
+            <div class="d-flex justify-center items-center mx-2">
+              <img
+                src="https://ik.imagekit.io/2nuimwatr/Component%201.png?updatedAt=1701851731230"
+                alt=""
+                class="mx-2"
+              />
+              <b class="text-[#454545]">Booking Request</b>
+            </div>
+            <h4 class="py-2 text-[#E57C35]">+011 4765 32568</h4>
+            <div class="d-flex justify-center items-center mx-2">
+              <img
+                src="https://ik.imagekit.io/2nuimwatr/Component%201%20(1).png?updatedAt=1701851908697"
+                alt=""
+                class="mx-2"
+              />
+              <b class="text-[#454545]">Location</b>
+            </div>
+            <p class="px-3 mx-3">
+              600 N Washington Ave Suite C203, Minneapolis, MN 55401
+            </p>
+            <div class="d-flex justify-center items-center mx-2">
+              <img
+                src="https://ik.imagekit.io/2nuimwatr/Component%202.png?updatedAt=1701851908726"
+                alt=""
+                class="mx-2"
+              />
+              <b class="text-[#454545]">Open Time</b>
+            </div>
+
+            <p class="px-3 ">Monday to Friday: 10:00 am - 10:30 pm</p>
+            <p class="px-3">Saturday to Sunday: 6:00 am - 11:00 pm</p>
+          </div>
+
+          <div className="col-lg-6 p-0 m-0" data-aos="fade-up">
+            <iframe
+              class="w-100 h-full"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+              frameBorder={0}
+              allowFullScreen
+            />
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+import Pizza from "./../Assets/1.png";
+import Wraps from "./../Assets/2.png";
+import Burgers from "./../Assets/3.png";
+import BubbleTea from "./../Assets/4.png";
+import Waffles from "./../Assets/5.png";
+import MILKSHAKES from "./../Assets/6.png";
+import Sundeas from "./../Assets/7.png";
+import CookieDough from "./../Assets/8.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Pizza",
+    image: Pizza,
+    desc: "15 items",
+  },
+  {
+    id: 2,
+    name: "Wraps",
+    image: Wraps,
+    desc: "13 items",
+  },
+  {
+    id: 3,
+    name: "Burgers",
+    image: Burgers,
+    desc: "8 items",
+  },
+  {
+    id: 4,
+    name: "Bubble Tea",
+    image: BubbleTea,
+    desc: "14 items",
+  },
+  {
+    id: 5,
+    name: "Waffles",
+    image: Waffles,
+    desc: "17 items",
+  },
+  {
+    id: 6,
+    name: "MILKSHAKES",
+    image: MILKSHAKES,
+    desc: "14 items",
+  },
+  {
+    id: 7,
+    name: "Sundeas",
+    image: Sundeas,
+    desc: "7 items",
+  },
+  {
+    id: 8,
+    name: "Cookie Dough",
+    image: CookieDough,
+    desc: "4 items",
+  }];
